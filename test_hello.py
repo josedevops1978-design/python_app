24fc94b33b663354d1e9c0395b5e71f8f85039d2
Jose Luis Albanes <jlalbanes@gmail.com>
2026-02-20 18:48:23 -0300

    Initial Commit

---
+++ test_hello.py
@@ -0,0 +1,6 @@
+from hello import app
+def test_home_route():
+   with app.test_client() as c:
+        response = c.get("/")
+        assert response.data == b"<p> Hello, world! </p>"
+        assert response.status_code == 200
